14be1bab39458c3b63ad1c3f107850d991ecc01d
MathisLec <mathis.lecoeuvre@gmail.com>
2024-07-22 23:49:00 +0200

    feat(fix): add argparse on server

--- C2/main.py
+++ C2/main.py
@@ -1,23 +1,35 @@
+import argparse
+
 from server import Server
 import sys
 
+SERVER_PORT = 0
+
+
+def update_server_port(args):
+    if args.port < 0 or args.port > 65535:
+        print("Port number needs to be between 0 and 65535")
+        exit(1)
+    global SERVER_PORT
+    SERVER_PORT = args.port
+
+
+def update_global_variables(args):
+    update_server_port(args)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Server for the C2')
+    parser.add_argument('--port', help='Server Port (default: 8888)', default=8888, type=int)
+    args = parser.parse_args()
+    update_global_variables(args)
+
+
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py <port>")
-        exit(1)
+    parse_args()
 
-    try:
-        port = int(sys.argv[1])
-        if port < 0 or port > 65535:
-            print("Port number needs to be between 0 and 65535")
-            exit(1)
-        
-    except ValueError:
-        print("Port number needs to be an Integer")
-        exit(1)
-
-    server = Server('0.0.0.0',port)
+    server = Server('0.0.0.0', SERVER_PORT)
 
     server.start()
 
--- Client/main.py
+++ Client/main.py
@@ -9,23 +9,28 @@ DEBUG_MODE = False
 SERVER_IP = ""
 SERVER_PORT = 0
 
+
 def update_debug(args):
     if args.debug:
         global DEBUG_MODE
         DEBUG_MODE = True
 
+
 def update_server_ip(args):
     global SERVER_IP
     SERVER_IP = args.ip
+
 
 def update_server_port(args):
     global SERVER_PORT
     SERVER_PORT = args.port
 
+
 def update_global_variables(args):
     update_debug(args)
     update_server_ip(args)
     update_server_port(args)
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Client for the C2')
@@ -38,6 +43,7 @@ def parse_args():
     print("DEBUG_MODE: %s" % DEBUG_MODE)
     print("SERVER_IP: %s" % SERVER_IP)
     print("SERVER_PORT: %d" % SERVER_PORT)
+
 
 if __name__ == "__main__":
     parse_args()
